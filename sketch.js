b034c19ad1f96f0d40f4b57f9dc633795451e42b
MoeDev123 <63486617+MoeDev123@users.noreply.github.com>
2020-07-19 15:04:26 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,23 +10,31 @@ let xDirectionBall = 1;
 let yDirectionBall = 1;
 
 
+
 function setup() {
     createCanvas(500,500);
     background(0);
 
     xSpeedBall = random(-5,5);
-    ySpeedBall = random(-5,5);
+    ySpeedBall = (-5,5);
 
 }
 
 
 function draw() { 
    background(0);
-   fill(0,0,255);
-   rect(xPos2,yPos2,25,100); 
+   fill(255,255,0)
+   //yellow blue side goal
+   rect(475,190,25,100)
+   //yellow red side goal
+   rect(0,190,25,100)
    fill(255);
+   //border line
    rect(245,-5,25,505)
-    
+    fill(0,0,255);
+   //blue player
+   rect(xPos2,yPos2,25,100); 
+   
     if (keyIsDown(UP_ARROW) && yPos2 >  0 ) {
     yPos2 -= 4;
 }
@@ -41,6 +49,7 @@ function draw() {
 }
  
     fill(255,0,0);
+    //red player
     rect(xPos,yPos,25,100);
     
 
@@ -57,10 +66,11 @@ function draw() {
         yPos += 4;
 }
 
+
 fill(0, 255, 0);
    ellipse(xPosBall, yPosBall, 50, 50);
 
-   xPosBall += xSpeedBall *xDirectionBall;
+   xPosBall += xSpeedBall * xDirectionBall;
    yPosBall += ySpeedBall * yDirectionBall;
 
     if (xPosBall < 25 || xPosBall > 475) {
@@ -69,14 +79,15 @@ fill(0, 255, 0);
     }
 
     if (yPosBall < 25 || yPosBall > 475) {
-        yDirectionBall *= -1
+        yDirectionBall *= -1;
 
     }
 
 
 
+    
 
-
+    
 
 
 
