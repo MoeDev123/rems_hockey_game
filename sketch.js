2f47fc1c68bf449e2d9fac3745a9cbc53541ecbe
MoeDev123 <63486617+MoeDev123@users.noreply.github.com>
2020-07-19 22:24:40 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,22 +2,39 @@ let xPos = 100
 let yPos = 100
 let xPos2 = 300
 let yPos2 = 400
+let xPosBall = 250;
+let yPosBall = 250;
+let xSpeedBall;
+let ySpeedBall; 
+let xDirectionBall = 1;
+let yDirectionBall = 1;
+
+
+
 function setup() {
     createCanvas(500,500);
     background(0);
+
+    xSpeedBall = random(-5,5);
+    ySpeedBall = (-5,5);
+
 }
 
-function mousa(){
-    
-}
 
 function draw() { 
    background(0);
-   fill(0,0,255);
-   rect(xPos2,yPos2,25,100); 
+   fill(255,255,0)
+   //yellow blue side goal
+   rect(475,190,25,100)
+   //yellow red side goal
+   rect(0,190,25,100)
    fill(255);
+   //border line
    rect(245,-5,25,505)
-    
+    fill(0,0,255);
+   //blue player
+   rect(xPos2,yPos2,25,100); 
+   
     if (keyIsDown(UP_ARROW) && yPos2 >  0 ) {
     yPos2 -= 4;
 }
@@ -32,6 +49,7 @@ function draw() {
 }
  
     fill(255,0,0);
+    //red player
     rect(xPos,yPos,25,100);
     
 
@@ -47,6 +65,31 @@ function draw() {
         if (keyIsDown(83) && yPos < 400 ) { //down
         yPos += 4;
 }
+
+
+fill(0, 255, 0);
+   ellipse(xPosBall, yPosBall, 50, 50);
+
+   xPosBall += xSpeedBall * xDirectionBall;
+   yPosBall += ySpeedBall * yDirectionBall;
+
+    if (xPosBall < 25 || xPosBall > 475) {
+        xDirectionBall *= -1;
+
+    }
+
+    if (yPosBall < 25 || yPosBall > 475) {
+        yDirectionBall *= -1;
+
+    }
+
+
+
+    
+
+    
+
+
 
 
 
